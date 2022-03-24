841c2a0b8031c33d040f1452bfd57fdc38fd4e5f
thetbn12 <thetbn12@gmail.com>
2022-03-24 16:52:25 -0400

    Initializing

---
+++ main.py
@@ -0,0 +1,169 @@
+"""
+HOW TO USE:
+Drag and drop G-Code file into the same folder as this program.
+Run the program.
+Set the file explorer path of G-Code file to read. 
+Set the file explorer path of G-Code file to write to.
+
+Console will print:
+- What lines edits were made at in both the original file and the new file
+- Number of lines read
+- Number of lines written
+- Total number of edits made
+"""
+
+readFileName = input("File Explorer path of G-Code file to read: ")
+writeFileName = input("File Explorer path of G-Code file to write to: ")
+print("")
+
+readFile = open(readFileName)
+writeFile = open(writeFileName, "w")
+
+# =============================================================================
+"""
+Delete any line that includes x:
+    "x",
+
+NOTES:
+- The more specific you are with x, the less likely the 
+    code will delete something you want to keep.  
+- Don't forget the comma at the end of the line!
+"""
+deleteLinesWith = [
+    "N40",
+    "N45",
+    ";(ORGX58=0,ORGY58=0,ORGZ58=0)",
+    "G43",
+]
+# =============================================================================
+"""
+Replace any entire line x with line y (and line z):
+    "x": "N y",
+    "x": "N y\n N z",
+    
+NOTES:
+- You can replace one line with multiple, just separate by "\n"
+- Gotta be real specific. If you're off by even a 
+    whitespace, software will note recognize it as the same line.
+- The "N" in front of the replacement line is important (sets up the line 
+    numbers in output file).
+- Don't forget the comma at the end of the line!
+"""
+replaceLinesWith = {
+    "N35 G53": "N## MCS Z0 \nN## MCS Y0",
+}
+# =============================================================================
+"""
+Replace any code x with y:
+    "x": "y",
+    
+NOTES:
+- Don't forget the comma at the end of the line!
+"""
+replaceCodes = {
+    "M3": "M3 M7",
+}
+# =============================================================================
+layerNumber = 0
+deckingSwitchNumber = 99999999
+lineCountRead = 0
+lineCountWrite = 5
+editCount = 0
+# -----------------------------------------------------------------------------
+def writeWithNum(string, num):
+    if string.startswith("N"):
+        stringParts = string.split()
+        fullString = ""
+        for part in stringParts:
+            if not part.startswith("N"):
+                fullString += part + " "
+        return "N" + str(num) + " " + fullString + "\n"
+    return string + "\n"
+# -----------------------------------------------------------------------------
+def consoleOut(numRead, numWrite):
+    print("Edited read file line " + str(numRead) + " to write file line "
+          + str(int((numWrite - 5) / 5)))
+# -----------------------------------------------------------------------------
+
+for nextLine in readFile:
+    lineCountRead += 1
+    # -------------------------------------------------------------------------
+    if nextLine.find("END"):
+
+        # Comment this line out if the flag line doesn't have G-Code
+        writeFile.write(writeWithNum(nextLine, lineCountWrite))
+        lineCountWrite += 5
+
+        if layerNumber < deckingSwitchNumber:
+            layerChangeGcode = [
+                "#PCALL decking.nc P1=V.P.layer_num",
+            ]
+        else: # Thinner decking pass G-Code
+            layerChangeGcode = [
+                "line 1",
+                "line 2",
+                "line 3",
+            ]
+
+        for nextCode in layerChangeGcode:
+            writeFile.write(writeWithNum(nextCode, lineCountWrite))
+            lineCountWrite += 5
+
+        consoleOut(lineCountRead, lineCountWrite)
+        editCount += 1
+        layerNumber += 1
+        continue;
+    # -------------------------------------------------------------------------
+    deleteLine = False
+    for deleteEntry in deleteLinesWith:
+        if nextLine.find(deleteEntry) >= 0:
+            writeFile.write("\n")
+            lineCountWrite += 5
+            consoleOut(lineCountRead, lineCountWrite)
+            editCount += 1
+            deleteLine = True
+            break
+    if deleteLine:
+        continue
+    # -------------------------------------------------------------------------
+    replaceLine = False
+    for replaceEntry in replaceLinesWith:
+        if nextLine == replaceEntry + "\n":
+            newLine = replaceLinesWith.get(replaceEntry)
+            splitLine = newLine.split("\n")
+            for sendLine in splitLine:
+                writeFile.write(writeWithNum(sendLine, lineCountWrite))
+                lineCountWrite += 5
+
+            consoleOut(lineCountRead, lineCountWrite)
+            editCount += 1
+            replaceLine = True
+            break
+    if replaceLine:
+        continue
+    # -------------------------------------------------------------------------
+    replaceCode = False
+    nextLine = nextLine.split()
+    for nextLineIndex in range(len(nextLine)):
+        for checkReplace in replaceCodes:
+            if nextLine[nextLineIndex] == checkReplace:
+                nextLine[nextLineIndex] = replaceCodes.get(checkReplace)
+                editCount += 1
+                replaceCode = True
+
+    fullLine = ""
+    for nextCode in nextLine:
+        fullLine += nextCode + " "
+
+    writeFile.write(writeWithNum(fullLine, lineCountWrite))
+    lineCountWrite += 5
+    if replaceCode:
+        consoleOut(lineCountRead, lineCountWrite)
+    # -------------------------------------------------------------------------
+
+print("\nLines read: " + str(lineCountRead))
+print("Lines Written: " + str(int((lineCountWrite - 5) / 5)))
+print("Edits made: " + str(editCount))
+
+readFile.close()
+writeFile.close()
---
+++ testOut.nc
@@ -0,0 +1,95 @@
+Threadmill_top
+
+line 1
+line 2
+line 3
+N25 ; T3 D=0.3937 CR=0 - ZMIN=-0.6348 - FLAT END MILL 
+line 1
+line 2
+line 3
+N45 #MCS OFF 
+line 1
+line 2
+line 3
+N65 #CS OFF 
+line 1
+line 2
+line 3
+N85 G70 G80 G90 
+line 1
+line 2
+line 3
+N105 G17 G40 
+line 1
+line 2
+line 3
+N125 G53 
+line 1
+line 2
+line 3
+N145 G0 G53 Z24 
+line 1
+line 2
+line 3
+N165 G53 
+line 1
+line 2
+line 3
+N185 ; 2D CONTOUR5 
+line 1
+line 2
+line 3
+N205 T3 M6 
+line 1
+line 2
+line 3
+N225 S20000 
+line 1
+line 2
+line 3
+N245 M3 
+line 1
+line 2
+line 3
+N265 G4 K3 
+line 1
+line 2
+line 3
+N285 G54 
+line 1
+line 2
+line 3
+;(ORGX58=0,ORGY58=0,ORGZ58=0)
+
+line 1
+line 2
+line 3
+N325 G58 
+line 1
+line 2
+line 3
+N345 D1 
+line 1
+line 2
+line 3
+N365 G0 X5.4291 Y3 
+line 1
+line 2
+line 3
+N385 G43 Z0.6 
+line 1
+line 2
+line 3
+
+
+line 1
+line 2
+line 3
+N425 M3 
+line 1
+line 2
+line 3
+N445 M30 
+line 1
+line 2
+line 3
